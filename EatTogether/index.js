8e115d3b0bbe07e9031b4fe249a9df89b16479e0
minhtuan97 <minhtuanle97@gmail.com>
2019-11-07 10:37:26 +0700

    login

--- EatTogether/index.js
+++ EatTogether/index.js
@@ -7,5 +7,7 @@ import App from './App';
 import {name as appName} from './app.json';
 import Onboarding from './screens/onboarding/Onboarding';
 import Lifecycle from './screens/Lifecycle/Lifecycle';
+import Loginscreen from './screens/login/login';
 
-AppRegistry.registerComponent(appName, () => Lifecycle);
+
+AppRegistry.registerComponent(appName, () => Loginscreen);
