a7103b4b49c7f4eac2e5396b974c838d5081ddab
minhtuan97 <minhtuanle97@gmail.com>
2019-11-09 16:19:00 +0700

    scrren chưa firebase

--- EatTogether/index.js
+++ EatTogether/index.js
@@ -3,11 +3,13 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import FirebaseLogin from './App';
 import {name as appName} from './app.json';
 import Onboarding from './screens/onboarding/Onboarding';
 import Lifecycle from './screens/Lifecycle/Lifecycle';
 import Loginscreen from './screens/login/login';
+import Login from './screens/login/index'
+import Register from './screens/singup/index'
+import ForgotPassword from './screens/forgotpassword/index'
 
-
-AppRegistry.registerComponent(appName, () => Loginscreen);
+AppRegistry.registerComponent(appName, () => FirebaseLogin);
